d2d5c7e09e9e78901fe7b1f64e7537af306f0d52
David Sanchez <davgdafa@gmail.com>
2021-08-17 17:14:53 -0400

    TEST-001: Simplified and refactored the project gradle files.

--- data/src/main/java/com/got/data/di/dataModule.kt
+++ data/src/main/java/com/got/data/di/dataModule.kt
@@ -13,11 +13,9 @@ import com.got.data.remote.GotCharactersService
 import com.got.data.remote.GotCharactersServiceImpl
 import com.got.domain.data.GotCharactersRepository
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 val dataModule = module {
@@ -29,8 +27,7 @@ val dataModule = module {
     factory<GotCharactersApiInterface> {
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://thronesapi.com/")
-            .client(OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }).build())
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .client(OkHttpClient.Builder().build())
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").create()))
             .build()
         retrofit.create(GotCharactersApiInterface::class.java)
